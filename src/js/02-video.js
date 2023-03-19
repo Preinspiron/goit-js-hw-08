5e7ba68b2f3150231d5462aa68b5bd688ade1413
V_Borysenko <preinspiron@gmail.com>
2023-03-19 19:58:59 +0100

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -7,22 +7,21 @@ const player = new Player(iframe);
 document.addEventListener(
   'DOMContentLoaded',
   () => {
-    player
-      .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
-      .then(function (seconds) {
-        // seconds = the actual time that the player seeked to
-      })
-      .catch(function (error) {
-        switch (error.name) {
-          case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
+    player.setCurrentTime(+localStorage.getItem('videoplayer-current-time'));
+    // .then(function (seconds) {
+    //   // seconds = the actual time that the player seeked to
+    // })
+    // .catch(function (error) {
+    //   switch (error.name) {
+    //     case 'RangeError':
+    //       // the time was less than 0 or greater than the video’s duration
+    //       break;
 
-          default:
-            // some other error occurred
-            break;
-        }
-      });
+    //     default:
+    //       // some other error occurred
+    //       break;
+    //   }
+    // });
   },
   { once: true }
 );
@@ -38,7 +37,6 @@ const saveLocalStorageTimeWithParamThrottle = throttle(
 player.on('timeupdate', saveLocalStorageTimeWithParamThrottle);
 
 function saveLocalStorageTime(sec) {
-  console.log(112);
   localStorage.setItem('videoplayer-current-time', sec);
 }
 
